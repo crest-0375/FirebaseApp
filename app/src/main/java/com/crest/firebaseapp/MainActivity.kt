6c05a6f3efb526eeb625fddf5f7e14bc7124674b
LAPTOP-VCF3S87N\JemishAKhunt <jemish.k@crestinfosystems.com>
2024-02-26 17:34:59 +0530

    Initial commit

---
+++ app/src/main/java/com/crest/firebaseapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.crest.firebaseapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.practice.firebaseapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
