63ae3f0b22b77b94c6a867b669ec6906f13ca9b9
LAPTOP-VCF3S87N\JemishAKhunt <jemish.k@crestinfosystems.com>
2024-02-27 09:48:31 +0530

    real time storage learned

--- app/src/main/java/com/crest/firebaseapp/MainActivity.kt
+++ app/src/main/java/com/crest/firebaseapp/MainActivity.kt
@@ -1,12 +1,44 @@
 package com.crest.firebaseapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.practice.firebaseapp.R
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.doOnTextChanged
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.practice.firebaseapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    val database: FirebaseDatabase = FirebaseDatabase.getInstance()
+    val reference: DatabaseReference = database.reference.child("Users")
+    val reference2: DatabaseReference = database.reference
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        reference2.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                binding.textView2.text = snapshot.child("Users").child("name").value.toString()
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+
+        })
+        binding.button.setOnClickListener {
+            val userName = binding.editTextText.text.toString()
+            reference.child("name").setValue(userName).addOnSuccessListener {
+                Log.d("TAG", "Added")
+            }
+                .addOnFailureListener {
+                    it.localizedMessage?.let { it1 -> Log.d("TAG", it1) }
+                }
+        }
     }
 }
